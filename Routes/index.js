614dc037e37b4df02844d6faf6910a0ab08a3262
Tim Hunt <timhunt@Tims-MBP.home>
2018-08-27 01:06:51 -0400

    message

--- Routes/index.js
+++ Routes/index.js
@@ -2,20 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 
-
-
-
-router.post('/', function (req, res) {
+router.get('/', function (req, res) {
     
-  
-    res.status(204).send("Made it")
+  res.send("Made it here ");
+
 });
 
 
 
-router.post('/translate', function (req, res) {
+router.get('/translate', function (req, res) {
     
-    const params = Object.assign(req.query, reqbody)
+    const params = Object.assign(req.query, req.body)
     console.log(params)
     res.status(204).send()
 });
--- server.js
+++ server.js
@@ -33,24 +33,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(__dirname + '/public'));
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
+app.use('/api',require('./routes/index'));
 
+var server = http.createServer(app);
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
-app.get('/',function(req,res){
-
-  res.send("HI");
-  console.log("hit man");
-
-});
-
-app.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
