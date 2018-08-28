cef1d045951f7175c80a0f3035a70f8750ad7cb1
Tim Hunt <timhunt@Tims-MBP.home>
2018-08-28 00:58:42 -0400

    maybe

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Nexmo = require('nexmo');
-const config = require('../config');
+const config = require('../config/config');
 
 
 router.get('/', function (req, res) {
