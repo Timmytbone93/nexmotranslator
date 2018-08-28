5af3d1897b66bcfbbf6d71bcebbf2be8bbe92350
Tim Hunt <timhunt@Tims-MBP.home>
2018-08-28 00:32:33 -0400

    got the ms key in nexmo object

--- Routes/index.js
+++ Routes/index.js
@@ -1,20 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const Nexmo = require('nexmo');
 
 
 router.get('/', function (req, res) {
     
-  res.send("Made it here ");
+    console.log("Hi");
+    res.send("Hi");
+
 
 });
 
 
 
 router.get('/translate', function (req, res) {
+
+    //this will simulate the sending of a tanslated sms via nexmo sms api.
+    const nexmo = new Nexmo({
+        apiKey: "d028ff0f",
+        apiSecret: "KOmzKrspClqfex03"//,
+        //msTranslateKey:"b548c1a406e14522912b451cdead22c4"
+      });
     
-    const params = Object.assign(req.query, req.body)
-    console.log(params)
-    res.status(204).send()
+     res.send("Translate");
+    console.log(nexmo);
+      //const translateSend =[{"":""}];
+
+      /*
+
+      nexmo.message.sendSms(
+        "19126420089", '17328535972', 'yo',
+          (err, responseData) => {
+            if (err) {
+              console.log(err);
+
+            } else {
+              console.dir(responseData);
+         
+            }
+          }
+       );
+       
+    */
+  
 });
 
 
--- server.js
+++ server.js
@@ -33,11 +33,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/api',require('./routes/index'));
+app.use('/',require('./routes/index'));
 
 var server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, function(){
+server.listen(3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
