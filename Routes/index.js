ef72bccdf322dd61da9b249dadff8d9c3dc9866a
Timmytbone93@gmail.com <timothyhunt1993@gmail.com>
2018-08-28 15:09:01 -0400

    psuhing final draft

--- Routes/index.js
+++ Routes/index.js
@@ -18,31 +18,31 @@ router.get('/translate', function (req, res) {
 
     //this will simulate the sending of a tanslated sms via nexmo sms api.
     const nexmo = new Nexmo({
-        apiKey: config.userData.apiKey,
-        apiSecret: config.userData.apiSecret,
-        msTranslateKey:config.userData.msTranslateKey
+        apiKey: config.userData.nexmo_api_key,
+        apiSecret: config.userData.nexmo_api_secret,
+        msft_key:config.userData.msft_key
       });
     
      res.send("Translate");
-    console.log(nexmo);
-      //const translateSend =[{"":""}];
 
-      /*
 
+      var options = {translateOptions:{"translate":true,"dest_lang":"es"}}
+     // console.log(nexmo);
+      
       nexmo.message.sendSms(
-        "19126420089", '17328535972', 'yo',
+        "19126420089", '17328535972', 'the cat is red',options,
           (err, responseData) => {
             if (err) {
-              console.log(err);
+              //console.log(err);
 
             } else {
-              console.dir(responseData);
+             // console.log(responseData);
          
             }
           }
        );
        
-    */
+    
   
 });
 
